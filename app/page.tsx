8d266b959ed46f2d859df46360f447edae46e7d1
TheTong25 <tong31973@gmail.com>
2026-01-02 17:47:05 +0700

    fix css file

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Page = () => {
-    const titleTxt ="font-bold text-2xl"
     return (
-        <div>
-            <h1 className={titleTxt}>Hello world</h1>
-        </div>
+        <section>
+            <h1 className='text-center font-bold text-4xl mt-5'>The Hub For Every Dev <br/> Event you Can&#39;t Miss</h1>
+            <p className='text-center mt-5'>Hackathons, Meetups, and Conferences, All in One Place</p>
+        </section>
     )
 }
 export default Page
